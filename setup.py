81e8cced395d67ab170a4f58154e8c8d3f8c6297
张金祥 <grape.zhang@kucoin.com>
2020-03-24 11:34:19 +0800

    Update document

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='python-kumex',
-    version='1.0.9',
+    version='v1.0.10',
     packages=['kumex', 'kumex/base_request', 'kumex/marke_data', 'kumex/trade', 'kumex/user',
               'kumex/websocket', 'kumex/ws_token'],
     license="MIT",
