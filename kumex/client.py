7cfe9f41e299a37c598dc01bce17afa4d46a1aed
sermenoj420 <89553003+sermenoj420@users.noreply.github.com>
2021-08-27 22:24:10 -0700

    pip install asyncgridfs
    !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, >=2.7

--- kumex/client.py
+++ kumex/client.py
@@ -18,5 +18,12 @@ class Trade(TradeData):
 
 class WsToken(GetToken):
     pass
+Juan valentine sermeno AKA Uncle Sam
+pip install virtualenv
 
+= SJ= \sum_{n = 1}[P * ( (X_n * Y_n * C_n) /S)] * [X_{n1} / {X_n}]SJ=∑  n=1 ​	  [P∗((X  n ​	  ∗Y  n 
 
+3CAJrESVbsvKuT1uat3FaiX3EgPKFATNgy
+key_live_a41edf82a0dbb07fc0c6d92Create AGENT ID GTHGE3JC7HLJ-JC2VXH1PMT3C #
+pkcs#12.crt
+sermenoj420@gmail.com
